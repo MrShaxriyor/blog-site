966c2952da603c3774cff27334a30b4ea4052e65
Mrshaxriyor <coder.eror@gmail.com>
2025-07-12 15:59:24 +0500

    vazifa

--- home/views.py
+++ home/views.py
@@ -7,6 +7,23 @@ from django.contrib.auth.models import User
 # def get_home(request):
 #     return render(request, 'index.html')
 
+# def get_home(request):
+#     categorys = Category.objects.all()
+#     news_list = News.objects.order_by('-id')[:4]
+#     return render(request, 'index.html', {'categorys': categorys,
+#         'news_list': news_list})
+
+
 def get_home(request):
-    categorys = Category.objects.all()
-    return render(request, 'index.html', {'categorys': categorys})
+    categories = Category.objects.all()
+    first_news = []
+
+    for cat in categories:
+        post = News.objects.filter(category=cat).first()
+        if post:
+            first_news.append(post)
+
+    return render(request, 'index.html', {
+        'categories': categories,
+        'first_news': first_news[-4:]
+    })
